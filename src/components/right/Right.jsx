4b57f7127f9f534fc1e37b86d0af7638e0dc772e
joseph.stallone@dreamorbit.com <joseph.stallone@dreamorbit.com>
2022-06-13 19:54:48 +0530

    resolved recursive numbers

--- src/components/right/Right.jsx
+++ src/components/right/Right.jsx
@@ -5,9 +5,9 @@ import { reset } from '../../redux/userSlice'
 import { useDispatch } from 'react-redux'
 const Right = () => {
     const dispatch = useDispatch()
-    let bill = Number(useSelector(state => state.user.bill));
-    let tip = (Number(useSelector(state => state.user.tip)).toFixed(2));
-    let btntip = (Number(useSelector(state => state.user.btntip)).toFixed(2));
+    let bill = Number(useSelector(state => state.user.bill))
+    let tip = Number(useSelector(state => state.user.tip))
+    let btntip = Number(useSelector(state => state.user.btntip))
     
     const handleReset = (e) => {
         e.preventDefault();
@@ -21,19 +21,19 @@ const Right = () => {
                 <div className="tip">
                     <h3 className="tipamt">Tip Amount</h3>
                     <h4 className="perperson">/ person</h4>
-                    <h4 className='billl1'>${(btntip)}</h4>
+                    <h4 className='billl1'>${(btntip).toFixed(2)}</h4>
                 </div>
                 <div className="total">
                     <h3 className="totall">Total</h3>
                     <h4 className="perpersonn">/ person</h4>
-                    <h4 className='tip111'>${bill + btntip}</h4> 
+                    <h4 className='tip111'>${(bill + btntip).toFixed(2)}</h4> 
                 </div>
             </div>
             <button className="reset" onClick={handleReset}>Reset</button>
-            {((tip*0.01) * bill) + bill}
+            {(((tip*0.01) * bill) + bill).toFixed(2)}
             
         </div>
-        {((tip*0.01) * bill)}
+        {((tip*0.01) * bill).toFixed(2)}
         </>
     )
 }
